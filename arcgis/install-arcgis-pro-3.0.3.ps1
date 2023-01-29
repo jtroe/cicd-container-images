96d8b2d84ec37594e16427cd16114745fc7e0c8a
Jay Roebuck <jtroe@users.noreply.github.com>
2023-01-29 12:39:49 -0500

    unblock files

--- arcgis/install-arcgis-pro-3.0.3.ps1
+++ arcgis/install-arcgis-pro-3.0.3.ps1
@@ -1,7 +1,10 @@
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($env:ARCGIS_PRO_3_CAB_URL, 'ArcGISPro.cab')
+Unblock-File ./ArcGISPro.cab
 $wc.DownloadFile($env:ARCGIS_PRO_3_CAB1_URL, 'ArcGISPro1.cab')
+Unblock-File ./ArcGISPro1.cab
 $wc.DownloadFile($env:ARCGIS_PRO_3_MSI_URL, 'ArcGISPro.msi')
+Unblock-File ./ArcGISPro.msi
     
 msiexec.exe /i ArcGISPro.msi /qb ALLUSERS=1 ACCEPTEULA=YES CHECKFORUPDATESATSTARTUP=0 LOCK_AUTH_SETTINGS=FALSE ESRI_LICENSE_HOST=@geosaurus.esri.com SOFTWARE_CLASS=PROFESSIONAL AUTHORIZATION_TYPE=CONCURRENT_USE
 rm ArcGISPro.cab
