40f1a494467a97c3e291b828e971120bafa7eed9
Jay Roebuck <jroebuck@esri.com>
2023-02-03 12:09:31 -0500

    try with better syntax

--- arcgis/install-arcgis-pro-3.0.3.ps1
+++ arcgis/install-arcgis-pro-3.0.3.ps1
@@ -2,10 +2,7 @@
 (Get-Service -name msiserver).Start()
 
 # install chocolatey
-RUN Set-ExecutionPolicy Bypass -Scope Process -Force; \
-  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; \
-  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
 # install desktop runtime dependency
 choco install dotnet-6.0-desktopruntime -y
